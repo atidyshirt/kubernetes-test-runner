3578c2b1fefac3c32725e4e108b2cd2dded1effc
jordanp <jordanpyott@gmail.com>
2026-01-29 20:13:57 +1300

    chore: fix structure and action

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { KubectlServiceManager } from './packages/devspace-testing-nodejs/src/kubectl-service-manager.js';
-export { TestContainer } from './packages/devspace-testing-nodejs/src/test-container.js';
-export { WiremockService } from './packages/devspace-testing-nodejs/src/wiremock-service.js';
-export { MongoService } from './packages/devspace-testing-nodejs/src/mongo-service.js';
